75769d495312a6cbaa984cd5623de2cff06de032
zammsara <saztaylor@uamv.edu.ni>
2024-11-03 17:24:16 -0600

    interfaces

---
+++ Interfaces/LogIn/LogIn/models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogIn.models
+{
+    public struct Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+        public string TipoUsuario { get; set; } // "cliente" o "colaborador"
+
+        public Usuario(string nombreUsuario, string contrasena, string tipoUsuario)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasena = contrasena;
+            TipoUsuario = tipoUsuario;
+        }
+
+        // Método para verificar si las credenciales son correctas
+        public bool VerificarCredenciales(string usuario, string contrasena)
+        {
+            return NombreUsuario == usuario && Contrasena == contrasena;
+        }
+    }
+}
